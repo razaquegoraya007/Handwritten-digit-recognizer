66229b3f2643606618c009df7cba672022a914fb
Razaque Goraya <arazaque.goraya@gmail.com>
2024-07-30 20:44:36 +0500

    done

--- gui_digit_recognizer.py
+++ gui_digit_recognizer.py
@@ -6,7 +6,7 @@ from PIL import ImageGrab, Image
 import numpy as np
 
 model = load_model('mnist.h5')
-
+# hello
 def predict_digit(img):
     #resize image to 28x28 pixels
     img = img.resize((28,28))
@@ -25,25 +25,25 @@ class App(tk.Tk):
         tk.Tk.__init__(self)
 
         self.x = self.y = 0
-        
+
         # Creating elements
         self.canvas = tk.Canvas(self, width=300, height=300, bg = "white", cursor="cross")
         self.label = tk.Label(self, text="Draw..", font=("Helvetica", 48))
-        self.classify_btn = tk.Button(self, text = "Recognise", command = self.classify_handwriting)   
+        self.classify_btn = tk.Button(self, text = "Recognise", command = self.classify_handwriting)
         self.button_clear = tk.Button(self, text = "Clear", command = self.clear_all)
-       
+
         # Grid structure
         self.canvas.grid(row=0, column=0, pady=2, sticky=W, )
         self.label.grid(row=0, column=1,pady=2, padx=2)
         self.classify_btn.grid(row=1, column=1, pady=2, padx=2)
         self.button_clear.grid(row=1, column=0, pady=2)
-        
+
         #self.canvas.bind("<Motion>", self.start_pos)
         self.canvas.bind("<B1-Motion>", self.draw_lines)
 
     def clear_all(self):
         self.canvas.delete("all")
-        
+
     def classify_handwriting(self):
         HWND = self.canvas.winfo_id()  # get the handle of the canvas
         rect = win32gui.GetWindowRect(HWND)  # get the coordinate of the canvas
@@ -59,6 +59,6 @@ class App(tk.Tk):
         self.y = event.y
         r=8
         self.canvas.create_oval(self.x-r, self.y-r, self.x + r, self.y + r, fill='black')
-       
+
 app = App()
 mainloop()
